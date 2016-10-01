d07f8bf50afbcdc9de1ae86157bf81e84fc2becd
Apachka <danky167@gmail.com>
2016-10-01 12:32:33 +0700

    class Check

---
+++ Check.java
@@ -0,0 +1,13 @@
+/**
+ * Created by student on 01.10.16.
+ */
+
+public class Check
+{
+    public static User checkAuthentication()
+    {
+        String login="", pass="";
+        if
+    }
+
+}
