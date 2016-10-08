4d787129f88ea2a09e939b68c65738ea8758fd7c
Apachka <danky167@gmail.com>
2016-10-08 10:28:19 +0700

    Concol'

--- Main.java
+++ Main.java
@@ -2,10 +2,14 @@
  * Created by student on 01.10.16.
  */
 
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        String a = "Login:";
+        System.out.println(a);
+        String b = "Password:";
+        System.out.println(b);
     }
 }
