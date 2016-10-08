694f5e3aa3d038c7f11ee581cccd62d853c03e40
Apachka <danky167@gmail.com>
2016-10-08 11:18:19 +0700

    Add users

--- Main.java
+++ Main.java
@@ -2,14 +2,16 @@
  * Created by student on 01.10.16.
  */
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        String a = "Login:";
-        System.out.println(a);
-        String b = "Password:";
-        System.out.println(b);
+        List<String> users = new ArrayList<String>();
+        users.add("John Doe");
+        users.add("Jane Row");
+        System.out.println("New Users: " + users);
     }
 }
