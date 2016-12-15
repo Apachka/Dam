cbba09a79e1f919672f29c5aeb7cc1f5cd1186cf
Apachka <danky167@gmail.com>
2016-12-16 00:03:54 +0700

    (+) +build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+source ./config.sh
+
+# Компиляция всех *.java файлов
+mkdir -p $OUT_CLS
+find . -name "*.java" | xargs javac -cp "$CP" -d $OUT_CLS -sourcepath $SRC -verbose
+
+# Копирование их из $RES в $OUT_CLS
+cp -r $RES $OUT_CLS
+
+# Копирование библиотек
+mkdir -p $OUT_LIB
+cp $LIB $OUT_LIB
+
+# Архивация всех классов в $OUT_JAR в jar файл, где $MAIN – класс с main()
+jar -cfe $OUT_JAR $MAIN -C $OUT_CLS .
